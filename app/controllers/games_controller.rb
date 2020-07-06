cb196bb6dbf2b661593a17632a89a683a6164a39
raphaelmantello <mantello.raphael@gmail.com>
2020-07-06 15:12:48 +0200

    Added game index view

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,46 @@
+class GamesController < ApplicationController
+  before_action :set_game, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @games = Game.all
+  end
+
+  def show
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.save
+
+    redirect_to game_path(@game)
+  end
+
+  def edit
+  end
+
+  def update
+    @game.update(game_params)
+
+    redirect_to game_path(@game)
+  end
+
+  def destroy
+    @game.destroy
+
+    redirect_to root_path
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :created_at)
+  end
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
+end
