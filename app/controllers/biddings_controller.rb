e2a5e6e6abc2215b521fd2f43a68379e5179c210
raphaelmantello <mantello.raphael@gmail.com>
2020-07-07 17:36:40 +0200

    Added biddings to game show - missing belote column

--- app/controllers/biddings_controller.rb
+++ app/controllers/biddings_controller.rb
@@ -1,2 +1,21 @@
 class BiddingsController < ApplicationController
+  def new
+    @game = Game.find(params[:game_id])
+    @bidding = Bidding.new
+  end
+
+  def create
+    @bidding = Bidding.new(bidding_params)
+    @game = Game.find(params[:game_id])
+    @bidding.game = @game
+    @bidding.save
+
+    redirect_to game_path(@game)
+  end
+
+  private
+
+  def bidding_params
+    params.require(:bidding).permit(:bid_points, :bid_color, :bid_team, :countered, :points)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :games do
-    resources :bidding, only: [:new, :create]
+    resources :biddings, only: [:new, :create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
