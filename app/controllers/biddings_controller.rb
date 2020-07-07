0cc1d4f996ff5435df758d3e215fec3c500b12d3
raphaelmantello <mantello.raphael@gmail.com>
2020-07-07 18:21:29 +0200

    game show back OK

--- app/controllers/biddings_controller.rb
+++ app/controllers/biddings_controller.rb
@@ -8,14 +8,17 @@ class BiddingsController < ApplicationController
     @bidding = Bidding.new(bidding_params)
     @game = Game.find(params[:game_id])
     @bidding.game = @game
-    @bidding.save
 
-    redirect_to game_path(@game)
+    if @bidding.save
+      redirect_to game_path(@game)
+    else
+      render :new
+    end
   end
 
   private
 
   def bidding_params
-    params.require(:bidding).permit(:bid_points, :bid_color, :bid_team, :countered, :points)
+    params.require(:bidding).permit(:bid_points, :bid_color, :bid_team, :belote, :countered, :points)
   end
 end
--- app/models/bidding.rb
+++ app/models/bidding.rb
@@ -1,3 +1,10 @@
 class Bidding < ApplicationRecord
   belongs_to :game
+
+  validates :bid_points, presence: true
+  validates :bid_color, presence: true
+  validates :bid_team, presence: true
+  validates :belote, presence: true
+  validates :countered, presence: true
+  validates :points, presence: true
 end
